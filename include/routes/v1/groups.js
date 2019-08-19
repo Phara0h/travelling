83de3fd46bfe217dd0d46a468fa26c3bd93042e5
Jt Whissel <jtwhissel@gmail.com>
2019-08-19 18:36:58 -0400

    [ENG-1037] - Got routing and auth up to being ready and added some user routes

--- include/routes/v1/groups.js
+++ include/routes/v1/groups.js
@@ -3,7 +3,7 @@
 const Group = require('../../database/models/group');
 
 module.exports = function(app, opts, done) {
-
+    const router = opts.router;
     app.put('/group', async (req, res) => {
         if (!req.body.name) {
             res.code(400).send(
@@ -16,7 +16,7 @@ module.exports = function(app, opts, done) {
             if(!req.body.type){
               req.body.type = 'group';
             }
-            
+
 
             var id = req.body.id ? {id: req.body.id} : {name: req.body.name};
 
@@ -36,6 +36,82 @@ module.exports = function(app, opts, done) {
             }
         }
     });
+
+    app.put('/group/:groupname', async (req, res) => {
+        if (!req.params.groupname) {
+            res.code(400).send(
+                {
+                    type: 'error',
+                    msg: 'A group requires a name',
+                });
+        } else {
+            var name = req.params.groupname.toLowerCase();
+            var group = await Group.findLimtedBy({name},'AND', 1);
+
+            if (group.length <= 0) {
+                res.code(400).send(
+                    {
+                        type: 'error',
+                        msg: 'No group with the name or id already exists.',
+                    });
+            } else {
+                group = group[0];
+                if(req.body.name) {
+                  group.name = req.body.name;
+                }
+                if(req.body.inherited) {
+                  group.inherited = req.body.inherited;
+                }
+                if(req.body.type) {
+                  group.type = req.body.type;
+                }
+                if(req.body.is_default) {
+                  group.is_default = Boolean(req.body.is_default);
+                }
+                if(req.body.allowed) {
+                  group.allowed = [];
+                  for (var i = 0; i < req.body.allowed.length; i++) {
+                    group.addRoute(req.body.allowed[i]);
+                  }
+                }
+                await group.save();
+
+                res.code(200).send(group);
+                router.needsGroupUpdate = true;
+            }
+        }
+    });
+
+    app.put('/group/:groupname/route', async (req, res) => {
+        if (!req.params.groupname) {
+            res.code(400).send(
+                {
+                    type: 'error',
+                    msg: 'A group requires a name',
+                });
+        } else {
+            var name = req.params.groupname.toLowerCase();
+            var group = await Group.findLimtedBy({name},'AND', 1);
+
+            if (group.length <= 0) {
+                res.code(400).send(
+                    {
+                        type: 'error',
+                        msg: 'No group with the name or id already exists.',
+                    });
+            } else {
+                group = group[0];
+
+                group.addRoute(req.body);
+                console.log(group)
+                await group.save();
+                console.log(group)
+                res.code(200).send(group);
+                router.needsGroupUpdate = true;
+            }
+        }
+    });
+
 
     app.get('/groups', async (req, res) => {
         res.send(await Group.findAll());
--- include/routes/v1/users.js
+++ include/routes/v1/users.js
@@ -1,3 +1,21 @@
-module.exports = function(fastify, opts, done) {
-    done();
+const fp = require('fastify-plugin')
+
+module.exports = function (app, opts, done){
+  const router = opts.router;
+
+  app.get('/user/me',(req, res) => {
+      res.send(req.session.user);
+  });
+
+  app.get('/user/me/route/allowed', async (req, res) => {
+      var group = router.groups[req.session.user.group.name];
+
+      res.send(req.session.user);
+  });
+
+  app.get('/user/me/permission/allowed/:permission',(req, res) => {
+      var isAllowed = router.isPermissionAllowed(req.params.permission, router.currentGroup(req,res), req.session.user);
+      res.code(isAllowed ? 200 : 401).send();
+  });
+  done();
 };
