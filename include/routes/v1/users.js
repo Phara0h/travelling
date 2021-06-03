506d0f917b5b36fbcbc17e1929fb499c4d5e465a
Clayton Ward <clayton@flaresoftware.com>
2021-06-03 08:30:51 -0700

    Created endpoint routes.

--- include/routes/v1/users.js
+++ include/routes/v1/users.js
@@ -325,6 +325,13 @@ function routes(app, opts, done) {
     return { count: 0 }
   });
 
+
+  app.get('/users/count', async (req, res) => {
+    // TODO: Get users count
+
+    return { count: 0 }
+  });
+
   app.get('/users/domain/:domain', async (req, res) => {
     if (!req.query.filter) {
       req.query.filter = 'domain=' + req.params.domain;
@@ -333,6 +340,18 @@ function routes(app, opts, done) {
     }
 
     return await User.findAllByFilter({ filter: req.query.filter, sort: req.query.sort, limit: req.query.limit, sortdir: req.query.sortdir });
+  });
+  
+  app.get('/users/domain/:domain/count', async (req, res) => {
+    // TODO: Get users count
+
+    if (!req.query.filter) {
+      req.query.filter = 'domain=' + req.params.domain;
+    } else {
+      req.query.filter += ',domain=' + req.params.domain;
+    }
+
+    return await User.getCountByFilter(req.query.filter)
   });
   
   app.get('/users/domain/:domain/count', async (req, res) => {
