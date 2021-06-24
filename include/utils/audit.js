0df59c786a8d8f8e3d6e9cfd3683c298eef74315
Clayton Ward <clayton@flaresoftware.com>
2021-06-24 15:02:00 -0700

    Created structure for creating audits.

---
+++ include/utils/audit.js
@@ -0,0 +1,20 @@
+// TODO: Create utils for adding audit logs
+//
+// function createAudit (opts)
+// opts: {
+//    action
+//    by_user
+//    of_user
+//    old_obj
+//    new_obj
+// }
+// Find users and pull remaining ids
+//
+// Split new / old changes into separate objs
+//
+// call insertAudit func
+// } 
+//
+// function insertAudit (entry)
+// inserts into db
+// 
