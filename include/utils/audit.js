d7eee435948e363cace2feb6a09ec05ec64c6086
Clayton Ward <clayton@flaresoftware.com>
2021-06-25 11:40:02 -0700

    Created audit util sorting and 'Edit' audit.

--- include/utils/audit.js
+++ include/utils/audit.js
@@ -18,3 +18,40 @@
 // function insertAudit (entry)
 // inserts into db
 // 
+
+function createAudit(opts) {
+    console.log('CREATE AUDIT...')
+    console.log(opts) 
+
+    for (i = 0; i < Object.keys(opts.newObj).length; i++) {
+        const prop = Object.keys(opts.newObj)[i];
+        const oldPropValue = opts.oldObj[prop];
+        const newPropValue = opts.newObj[prop];
+
+        if (newPropValue !== oldPropValue) {
+            insertAudit({
+                action: opts.action,
+                by_user_id: opts.byUser.id,
+                by_user_email: opts.byUser.email,
+                by_user_username: opts.byUser.username,
+                of_user_id: opts.ofUser.id,
+                of_user_email: opts.ofUser.email,
+                of_user_username: opts.ofUser.username,
+                prop: prop,
+                old_val: oldPropValue,
+                new_val: newPropValue,
+            });
+        }
+    }
+}
+
+function insertAudit(fields) {
+    // Make query to insert audit
+    console.log('INSERTING INTO DB...');
+    console.log(fields);
+}
+
+
+module.exports = {
+    createAudit
+}
