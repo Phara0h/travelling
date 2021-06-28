71348ed5257a260db7e966323e1f28b38a0d02e6
Clayton Ward <clayton@flaresoftware.com>
2021-06-28 08:49:56 -0700

    DB inserts for audits. Added Delete audits.

--- include/utils/audit.js
+++ include/utils/audit.js
@@ -1,56 +1,51 @@
-// TODO: Create utils for adding audit logs
-//
-// function createAudit (opts)
-// opts: {
-//    action
-//    by_user
-//    of_user
-//    old_obj
-//    new_obj
-// }
-// Find users and pull remaining ids
-//
-// Split new / old changes into separate objs
-//
-// call insertAudit func
-// } 
-//
-// function insertAudit (entry)
-// inserts into db
-// 
+const Audit = require('../database/models/audit');
+const config = require('./config');
 
 function createAudit(opts) {
-    console.log('CREATE AUDIT...')
-    console.log(opts) 
+    switch (opts.action) {
+        case 'Edit':
+            editAction(opts);
+            break;
+        case 'Delete':
+            deleteAction(opts);
+            break;
+        default:
+            config.log.logger.debug(`Unknown audit action: ${opts.action}`);
+    }
+}
 
+async function editAction(opts) {
     for (i = 0; i < Object.keys(opts.newObj).length; i++) {
         const prop = Object.keys(opts.newObj)[i];
         const oldPropValue = opts.oldObj[prop];
         const newPropValue = opts.newObj[prop];
 
         if (newPropValue !== oldPropValue) {
-            insertAudit({
-                action: opts.action,
-                by_user_id: opts.byUser.id,
-                by_user_email: opts.byUser.email,
-                by_user_username: opts.byUser.username,
-                of_user_id: opts.ofUser.id,
-                of_user_email: opts.ofUser.email,
-                of_user_username: opts.ofUser.username,
-                prop: prop,
-                old_val: oldPropValue,
-                new_val: newPropValue,
-            });
+            const newAudit = createAuditObject(opts, prop, oldPropValue, newPropValue);
+            await Audit.create(newAudit);
         }
     }
 }
 
-function insertAudit(fields) {
-    // Make query to insert audit
-    console.log('INSERTING INTO DB...');
-    console.log(fields);
+async function deleteAction(opts) {
+    const newAudit = createAuditObject(opts);
+    await Audit.create(newAudit);
 }
 
+function createAuditObject(opts, prop, oldPropValue, newPropValue) {
+    return {
+        action: opts.action,
+        by_user_id: opts.byUser.id,
+        by_user_email: opts.byUser.email,
+        by_user_username: opts.byUser.username,
+        of_user_id: opts.ofUser.id,
+        of_user_email: opts.ofUser.email,
+        of_user_username: opts.ofUser.username,
+        prop: prop,
+        old_val: oldPropValue,
+        new_val: newPropValue
+    }
+}
 
 module.exports = {
     createAudit
