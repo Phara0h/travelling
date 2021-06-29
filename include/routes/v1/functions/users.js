5c7020f9f78b0902eda42c5d18ff3afe2e25ed74
Clayton Ward <clayton@flaresoftware.com>
2021-06-29 08:48:26 -0700

    Add support for audit environment variables.

--- include/routes/v1/functions/users.js
+++ include/routes/v1/functions/users.js
@@ -9,42 +9,42 @@ const Database = require('../../../database');
 const User = require('../../../database/models/user');
 
 async function deleteUser(opts) {
-    var id = _getId(opts.req);
-    var domain = opts.req.params.domain;
-    var user;
-  
-    if (!id) {
-      opts.res.code(400);
-      return {
-        type: 'user-find-by-error',
-        msg: 'No user by that username or id was found.'
-      };
-    }
-  
-    if (opts.needsDomain && !domain) {
-      opts.res.code(400);
-      return {
-        type: 'user-missing-param-error',
-        msg: 'No domain was provided.'
-      };
-    }
-  
-    if (opts.needsDomain) {
-      user = await User.deleteAllBy({ domain, ...id }, 'AND', 1);
-    } else {
-      user = await User.deleteAllBy(id, 'AND');
-    }
-  
-    if (user && user.length > 0) {
-      await user[0].resolveGroup(opts.router);
-  
-      var session = await opts.req.sessionStore.get(user[0].id);
-  
-      if (session) {
-        await opts.req.sessionStore.destroy(session.sessionId);
-      }
+  var id = _getId(opts.req);
+  var domain = opts.req.params.domain;
+  var user;
 
-      // Add to audit
+  if (!id) {
+    opts.res.code(400);
+    return {
+      type: 'user-find-by-error',
+      msg: 'No user by that username or id was found.'
+    };
+  }
+
+  if (opts.needsDomain && !domain) {
+    opts.res.code(400);
+    return {
+      type: 'user-missing-param-error',
+      msg: 'No domain was provided.'
+    };
+  }
+
+  if (opts.needsDomain) {
+    user = await User.deleteAllBy({ domain, ...id }, 'AND', 1);
+  } else {
+    user = await User.deleteAllBy(id, 'AND');
+  }
+
+  if (user && user.length > 0) {
+    await user[0].resolveGroup(opts.router);
+
+    var session = await opts.req.sessionStore.get(user[0].id);
+
+    if (session) {
+      await opts.req.sessionStore.destroy(session.sessionId);
+    }
+
+    if (config.audit.delete.enable) {
       audit.createAudit({ 
         action: 'Delete', 
         byUser: {
@@ -58,101 +58,102 @@ async function deleteUser(opts) {
           username: user[0].username
         }
       });
-  
-      opts.res.code(200);
-      return user[0];
     }
+
+    opts.res.code(200);
+    return user[0];
+  }
+
+  opts.res.code(400);
+  return {
+    type: 'user-delete-error',
+    msg: `No user by that id, username, email ${opts.needsDomain ? 'with that domain ' : ''}was found.`
+  };
+}
   
+async function editUser(opts) {
+  var id = _getId(opts.req);
+  var domain = opts.req.params.domain;
+
+  if (!id) {
     opts.res.code(400);
     return {
-      type: 'user-delete-error',
-      msg: `No user by that id, username, email ${opts.needsDomain ? 'with that domain ' : ''}was found.`
+      type: 'user-find-by-error',
+      msg: 'No user by that username or id was found.'
     };
   }
-  
-  async function editUser(opts) {
-    var id = _getId(opts.req);
-    var domain = opts.req.params.domain;
-  
-    if (!id) {
-      opts.res.code(400);
-      return {
-        type: 'user-find-by-error',
-        msg: 'No user by that username or id was found.'
-      };
-    }
-  
-    if (opts.needsDomain && !domain) {
-      opts.res.code(400);
-      return {
-        type: 'user-missing-param-error',
-        msg: 'No domain was provided.'
-      };
-    }
-  
-    // filter group_id
-    opts.req.body = filterUser(opts.req);
-  
-    var model = opts.req.body;
-    var oldModel = await getUser(opts);
 
-    if (opts.req.params.prop) {
-      model = opts.req.params.propdata
-        ? { [opts.req.params.prop]: opts.req.params.propdata }
-        : { [opts.req.params.prop]: opts.req.body };
+  if (opts.needsDomain && !domain) {
+    opts.res.code(400);
+    return {
+      type: 'user-missing-param-error',
+      msg: 'No domain was provided.'
+    };
+  }
 
-      oldModel = { [opts.req.params.prop]: oldModel }
-    }
-    
-    var isValid = await userUtils.checkValidUser(model);
+  // filter group_id
+  opts.req.body = filterUser(opts.req);
+
+  var model = opts.req.body;
+  var oldModel = await getUser(opts);
+
+  if (opts.req.params.prop) {
+    model = opts.req.params.propdata
+      ? { [opts.req.params.prop]: opts.req.params.propdata }
+      : { [opts.req.params.prop]: opts.req.body };
+
+    oldModel = { [opts.req.params.prop]: oldModel }
+  }
   
-    if (isValid === true) {
-      isValid = await Database.checkDupe(model);
-    }
-    if (isValid !== true) {
-      opts.res.code(400);
-      return isValid;
-    }
-  
-    var updatedProps = userUtils.setUser({}, model);
-  
-    if (misc.isEmpty(updatedProps)) {
+  var isValid = await userUtils.checkValidUser(model);
+
+  if (isValid === true) {
+    isValid = await Database.checkDupe(model);
+  }
+  if (isValid !== true) {
+    opts.res.code(400);
+    return isValid;
+  }
+
+  var updatedProps = userUtils.setUser({}, model);
+
+  if (misc.isEmpty(updatedProps)) {
+    opts.res.code(400);
+    return {
+      type: 'user-prop-error',
+      msg: 'Not a property of user'
+    };
+  }
+
+  var user;
+
+  if (opts.needsDomain) {
+    user = await User.updateLimitedBy({ domain, ...id }, updatedProps, 'AND', 1);
+  } else {
+    user = await User.updateLimitedBy(id, updatedProps, 'AND', 1);
+  }
+
+  if (user && user.length > 0) {
+    if (opts.req.params.prop && user[0][opts.req.params.prop] === undefined) {
       opts.res.code(400);
       return {
         type: 'user-prop-error',
         msg: 'Not a property of user'
       };
     }
-  
-    var user;
-  
-    if (opts.needsDomain) {
-      user = await User.updateLimitedBy({ domain, ...id }, updatedProps, 'AND', 1);
-    } else {
-      user = await User.updateLimitedBy(id, updatedProps, 'AND', 1);
+
+    const groupsData = await user[0].resolveGroup(opts.router);
+
+    opts.res.code(200);
+    // Update any current logged in users
+    var session = await opts.req.sessionStore.get(user[0].id);
+
+    if (session) {
+      session.data = { user: user[0], groupsData };
+      await opts.req.sessionStore.set(session.sessionId, session);
     }
 
-    if (user && user.length > 0) {
-      if (opts.req.params.prop && user[0][opts.req.params.prop] === undefined) {
-        opts.res.code(400);
-        return {
-          type: 'user-prop-error',
-          msg: 'Not a property of user'
-        };
-      }
-  
-      const groupsData = await user[0].resolveGroup(opts.router);
-  
-      opts.res.code(200);
-      // Update any current logged in users
-      var session = await opts.req.sessionStore.get(user[0].id);
-  
-      if (session) {
-        session.data = { user: user[0], groupsData };
-        await opts.req.sessionStore.set(session.sessionId, session);
-      }
-
-      // Add changes to audit
+    if (config.audit.edit.enable) {
       audit.createAudit({ 
         action: 'Edit', 
         byUser: {
@@ -168,107 +169,109 @@ async function deleteUser(opts) {
         oldObj: oldModel,
         newObj: model 
       });
-  
-      return opts.req.params.prop ? user[0][opts.req.params.prop] : user[0];
     }
-  
-    opts.res.code(400);
-    return {
-      type: 'user-edit-error',
-      msg: `No user by that id, username, email ${opts.needsDomain ? 'with that domain ' : ''}was found.`
-    };
+
+    return opts.req.params.prop ? user[0][opts.req.params.prop] : user[0];
   }
+
+  opts.res.code(400);
+  return {
+    type: 'user-edit-error',
+    msg: `No user by that id, username, email ${opts.needsDomain ? 'with that domain ' : ''}was found.`
+  };
+}
   
-  async function getUser(opts) {
-    var id = _getId(opts.req);
-    var domain = opts.req.params.domain;
-    var user;
-  
-    if (!id) {
-      opts.res.code(400);
-      return {
-        type: 'user-find-by-error',
-        msg: 'No user by that username or id was found.'
-      };
-    }
-  
-    if (opts.needsDomain && !domain) {
-      opts.res.code(400);
-      return {
-        type: 'user-missing-param-error',
-        msg: 'No domain was provided.'
-      };
-    }
-  
-    if (opts.needsDomain) {
-      user = await User.findLimtedBy({ domain, ...id }, 'AND', 1);
-    } else {
-      user = await User.findLimtedBy(id, 'AND', 1);
-    }
-  
-    if (user && user.length > 0) {
-      if (opts.req.params.prop && user[0][opts.req.params.prop] === undefined) {
-        opts.res.code(400);
-        return {
-          type: 'user-prop-error',
-          msg: 'Not a property of user'
-        };
-      }
-  
-      await user[0].resolveGroup();
-  
-      opts.res.code(200);
-      return opts.req.params.prop ? user[0][opts.req.params.prop] : user[0];
-    }
-  
+async function getUser(opts) {
+  var id = _getId(opts.req);
+  var domain = opts.req.params.domain;
+  var user;
+
+  if (!id) {
     opts.res.code(400);
     return {
       type: 'user-find-by-error',
-      msg: `No user by that id, username, email ${opts.needsDomain ? 'with that domain ' : ''}was found.`
+      msg: 'No user by that username or id was found.'
     };
   }
-  async function updateSessionUser(user, req) {
-    const groupsData = await user.resolveGroup();
-    // Update any current logged in users
-    var session = await req.sessionStore.get(user.id);
-  
-    if (session) {
-      session.data = { user, groupsData };
-      await req.sessionStore.set(session.sessionId, session);
-    }
+
+  if (opts.needsDomain && !domain) {
+    opts.res.code(400);
+    return {
+      type: 'user-missing-param-error',
+      msg: 'No domain was provided.'
+    };
   }
-  
-  async function getGroup(req, res) {
-    var group = null;
-  
-    if (req.params.groupid) {
-      group = await gm.getGroup(req.params.groupid, req.params.grouptype || 'group');
-    }
-  
-    if (!group) {
+
+  if (opts.needsDomain) {
+    user = await User.findLimtedBy({ domain, ...id }, 'AND', 1);
+  } else {
+    user = await User.findLimtedBy(id, 'AND', 1);
+  }
+
+  if (user && user.length > 0) {
+    if (opts.req.params.prop && user[0][opts.req.params.prop] === undefined) {
+      opts.res.code(400);
       return {
-        type: 'user-edit-group-error',
-        msg: 'No group with that type by that name or id was found.'
+        type: 'user-prop-error',
+        msg: 'Not a property of user'
       };
     }
-  
-    return group;
-  }
-  
-  async function addRemoveGroupInheritance(user, group, add = true, req) {
-    if (user) {
-      user = add ? await user.addGroup(group) : await user.removeGroup(group);
-  
-      if (!user) {
-        return {
-          type: `user-${add ? 'add' : 'remove'}-group-error`,
-          msg: `User could not ${add ? 'add' : 'remove'} group.`
-        };
-      }
-      await updateSessionUser(user, req);
 
-      // Add changes to audit
-      audit.createAudit({ 
+    await user[0].resolveGroup();
+
+    opts.res.code(200);
+    return opts.req.params.prop ? user[0][opts.req.params.prop] : user[0];
+  }
+
+  opts.res.code(400);
+  return {
+    type: 'user-find-by-error',
+    msg: `No user by that id, username, email ${opts.needsDomain ? 'with that domain ' : ''}was found.`
+  };
+}
+
+async function updateSessionUser(user, req) {
+  const groupsData = await user.resolveGroup();
+  // Update any current logged in users
+  var session = await req.sessionStore.get(user.id);
+
+  if (session) {
+    session.data = { user, groupsData };
+    await req.sessionStore.set(session.sessionId, session);
+  }
+}
+
+async function getGroup(req, res) {
+  var group = null;
+
+  if (req.params.groupid) {
+    group = await gm.getGroup(req.params.groupid, req.params.grouptype || 'group');
+  }
+
+  if (!group) {
+    return {
+      type: 'user-edit-group-error',
+      msg: 'No group with that type by that name or id was found.'
+    };
+  }
+
+  return group;
+}
+
+async function addRemoveGroupInheritance(user, group, add = true, req) {
+  if (user) {
+    user = add ? await user.addGroup(group) : await user.removeGroup(group);
+
+    if (!user) {
+      return {
+        type: `user-${add ? 'add' : 'remove'}-group-error`,
+        msg: `User could not ${add ? 'add' : 'remove'} group.`
+      };
+    }
+    await updateSessionUser(user, req);
+
+    if (config.audit.edit.enable) {
+        audit.createAudit({ 
         action: add ? 'AddGroupInheritance' : 'RemoveGroupInheritance', 
         byUser: {
           id: req.session.data.user.id,
@@ -283,56 +286,58 @@ async function deleteUser(opts) {
         oldObj: add ? '' : group.id,
         newObj: add ? group.id : '',
       });
+    }
 
-      return user;
-    }
-  
-    return {
-      type: 'user-edit-error',
-      msg: 'No user by that username or id was found.'
-    };
+    return user;
   }
-  function filterUser(req) {
-    if (!req.body) {
-      return req.body;
-    }
-  
-    if (req.body.group_ids) {
-      // if (req.params.prop == 'group_ids' && req.params.propdata) {
-      // req.body.group_ids = req.params.propdata;
-      // } else {
-      delete req.body.group_ids;
-      // }
-    }
-  
-    if (config.user.isolateByDomain) {
-      if (req.body.domain) {
-        delete req.body.domain;
-      }
-    }
-  
+
+  return {
+    type: 'user-edit-error',
+    msg: 'No user by that username or id was found.'
+  };
+}
+
+function filterUser(req) {
+  if (!req.body) {
     return req.body;
   }
 
-  function _getId(req) {
-    if (!req.params.id) {
+  if (req.body.group_ids) {
+    // if (req.params.prop == 'group_ids' && req.params.propdata) {
+    // req.body.group_ids = req.params.propdata;
+    // } else {
+    delete req.body.group_ids;
+    // }
+  }
+
+  if (config.user.isolateByDomain) {
+    if (req.body.domain) {
+      delete req.body.domain;
+    }
+  }
+
+  return req.body;
+}
+
+function _getId(req) {
+  if (!req.params.id) {
+    return null;
+  }
+  // if prob an email addresss
+  if (req.params.id.indexOf('@') > -1) {
+    return { email: req.params.id };
+  }
+
+  if (!regex.uuidCheck(req.params.id)) {
+    if (regex.username.exec(req.params.id)) {
+      return { username: req.params.id };
+    } else {
       return null;
     }
-    // if prob an email addresss
-    if (req.params.id.indexOf('@') > -1) {
-      return { email: req.params.id };
-    }
-  
-    if (!regex.uuidCheck(req.params.id)) {
-      if (regex.username.exec(req.params.id)) {
-        return { username: req.params.id };
-      } else {
-        return null;
-      }
-    }
-  
-    return { id: req.params.id };
   }
+
+  return { id: req.params.id };
+}
 
   
 module.exports = { 
