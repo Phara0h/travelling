1eb9fd9546a38e79e7b5648b252d061e1b9eb913
Clayton Ward <clayton@flaresoftware.com>
2021-07-13 15:57:47 -0700

    Created audit retrieval end points.

--- include/routes/v1/functions/audit.js
+++ include/routes/v1/functions/audit.js
@@ -1,1 +1,41 @@
-// TODO: Audit functions
+const Audit = require('../../../database/models/audit');
+
+async function getAuditsByUserID(opts) {
+    const id = opts.req.params.id;
+
+    if (!id) {
+        opts.res.code(400);
+        return {
+            type: 'missing-param-error',
+            msg: 'Please provide required parameter(s).'
+        };
+    }
+
+    // Strip filter white space
+    if (opts.req.query.filter && opts.req.query.filter.indexOf(' ') > -1) {
+        opts.req.query.filter = opts.req.query.filter.replace(/\s/g, '');
+    }
+
+    try {
+        return await Audit.findAllByIdWithFilter({
+            id: id,
+            byUser: opts.byUser,
+            ofUser: opts.ofUser,
+            filter: opts.req.query.filter,
+            limit: opts.req.query.limit,
+            skip: opts.req.query.skip,
+            sort: opts.req.query.sort,
+            sortdir: opts.req.query.sortdir,
+        });
+    } catch (e) {
+        opts.res.code(400);
+        return {
+            type: 'audit-filter-error',
+            msg: 'Invalid filter or other query param.'
+        };
+    }
+}
+
+module.exports = {
+    getAuditsByUserID
+}
