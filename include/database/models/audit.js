0ec25b38bda2984553539e24cf08d360fcdbdd26
Clayton Ward <clayton@flaresoftware.com>
2021-07-14 13:45:35 -0700

    Moved query logic to util file.

--- include/database/models/audit.js
+++ include/database/models/audit.js
@@ -1,8 +1,7 @@
 const BaseModel = require('adost').PGActiveModel;
 const Base = require('adost').Base;
 const PGTypes = require('adost').PGTypes;
-const regex = require('../../utils/regex');
-const misc = require('../../utils/misc');
+const { findAllByFilterQuery } = require('../../utils/query');
 
 class Audit extends Base(BaseModel, 'audits', {
   id: PGTypes.PK,
@@ -40,127 +39,8 @@ class Audit extends Base(BaseModel, 'audits', {
                     );`);
   }
 
-
-  // TODO: Extend Active model instead of this and the same function in User model
-  static async findAllByIdWithFilter(opts) {
-    var query = `SELECT * FROM ${this.table} `;
-    var keys = [];
-    var values = [];
-
-    if (opts.byUser === true) {
-      query += `WHERE by_user_id = '${opts.id}' `
-    }
-
-    if (opts.ofUser === true) {
-      query += `WHERE of_user_id = '${opts.id}' `
-    }
-
-    if (!opts.sortdir) {
-      opts.sortdir = 'DESC';
-    }
-
-    if (opts.limit && isNaN(opts.limit)) {
-      throw new Error('Invalid filter') 
-    }
-    opts.limit = Number.parseInt(opts.limit);
-
-    if (opts.skip && isNaN(opts.skip)) {
-      throw new Error('Invalid filter')
-    }
-    opts.skip = Number.parseInt(opts.skip);
-
-    if (opts.filter) {
-      if (opts.filter.indexOf(',') > -1) {
-        opts.filter = opts.filter.split(',');
-      } else {
-        opts.filter = [opts.filter];
-      }
-
-      var audit = {};
-      var ops = [];
-
-      for (var i = 0; i < opts.filter.length; i++) {
-        var op = '=';
-        var key;
-        var value;
-
-        if (opts.filter[i].indexOf('>=') > -1) {
-          var kv = opts.filter[i].split('>=');
-
-          op = '>=';
-          key = kv[0];
-          value = kv[1];
-        } else if (opts.filter[i].indexOf('<=') > -1) {
-          var kv = opts.filter[i].split('<=');
-
-          op = '<=';
-          key = kv[0];
-          value = kv[1];
-        } else if (opts.filter[i].indexOf('>') > -1) {
-          var kv = opts.filter[i].split('>');
-
-          op = '>';
-          key = kv[0];
-          value = kv[1];
-        } else if (opts.filter[i].indexOf('<') > -1) {
-          var kv = opts.filter[i].split('<');
-
-          op = '<';
-          key = kv[0];
-          value = kv[1];
-        } else if (opts.filter[i].indexOf('=') > -1) {
-          var kv = opts.filter[i].split('=');
-
-          key = kv[0];
-          value = kv[1];
-        }
-
-        if (this._defaultModel[key] !== undefined) {
-          if (this._encryptionFields[key] !== undefined) {
-            value = (await this._queryFieldsHash({ [key]: value }))['__' + key];
-            key = '__' + key;
-          }
-
-          ops.push(op);
-          values.push(value)
-          keys.push(key)
-          audit[key] = misc.stringToNativeType(value);
-        }
-      }
-
-      for (var i = 0; i < keys.length; i++) {
-        if (i === 0) {
-          query += ' AND ';
-        }
-        query += `"${keys[i]}"${ops[i]}$${i + 1} `;
-
-        if (keys.length > i + 1) {
-          query += ' AND ';
-        }
-      }
-    }
-
-    if (opts.sort && regex.safeName.exec(opts.sort) != null) {
-      query += ' ORDER BY ' + opts.sort + ' ' + (opts.sortdir == 'ASC' ? 'ASC' : 'DESC');
-    }
-
-    if (opts.limit && !opts.count) {
-      query += ' LIMIT ' +  opts.limit;
-    }
-
-    if (opts.skip && !opts.count) {
-      query += ' OFFSET ' +  opts.skip;
-    }
- 
-    const audits = await this.query(query, values);
-
-    for (var i = 0; i < audits.length; i++) {
-      if (audits[i]) {
-        audits[i] = await this.decrypt(audits[i], this.getEncryptedProfile(audits[i]), true);
-        delete audits[i].eprofile;
-      }
-    }
-    return audits;
+  static async findAllByFilter(opts) {
+    return await findAllByFilterQuery(this, opts);
   }
 }
 
--- include/routes/v1/audit.js
+++ include/routes/v1/audit.js
@@ -1,12 +1,25 @@
 const auditRoutes = require('./functions/audit');
 
 module.exports = function (app, opts, done) {
-    app.get('/audit/user/byuser/:id', async (req, res) => {
-        return await auditRoutes.getAuditsByUserID({ req, res, byUser: true });
-    });
-    app.get('/audit/user/ofuser/:id', async (req, res) => {
-        return await auditRoutes.getAuditsByUserID({ req, res, ofUser: true });
-    });
+  app.get('/audit/user/byuser/:id', async (req, res) => {
+    if (!req.query.filter) {
+        req.query.filter = `by_user_id=${req.params.id}`;
+      } else {
+        req.query.filter += `,by_user_id=${req.params.id}`;
+      }
+      
+    return await auditRoutes.getAuditsByUserID({ req, res, byUser: true });
+  });
+  
+  app.get('/audit/user/ofuser/:id', async (req, res) => {
+    if (!req.query.filter) {
+        req.query.filter = `of_user_id=${req.params.id}`;
+      } else {
+        req.query.filter += `,of_user_id=${req.params.id}`;
+      }
 
-    done();
+    return await auditRoutes.getAuditsByUserID({ req, res, ofUser: true });
+  });
+
+  done();
 };
