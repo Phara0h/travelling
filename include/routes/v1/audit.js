55a3cc3f459f1721aa75417c51a2c2002a064387
Clayton Ward <clayton@flaresoftware.com>
2021-07-22 15:52:16 -0700

    Added filters to action and subaction audit endpoints.

--- include/routes/v1/audit.js
+++ include/routes/v1/audit.js
@@ -2,35 +2,104 @@ const auditRoutes = require('./functions/audit');
 
 module.exports = function (app, opts, done) {
   app.get('/audit/user/byuser/:id', async (req, res) => {
-    if (!req.query.filter) {
-      req.query.filter = `by_user_id=${req.params.id}`;
-    } else {
-      req.query.filter += `,by_user_id=${req.params.id}`;
+    const id = req.params.id;
+
+    if (!id) {
+      res.code(400);
+      return {
+        type: 'missing-param-error',
+        msg: 'Please provide required parameter(s).'
+      };
     }
 
-    return await auditRoutes.getAuditsByUserID({ req, res, byUser: true });
+    if (!req.query.filter) {
+      req.query.filter = `by_user_id=${id}`;
+    } else {
+      req.query.filter += `,by_user_id=${id}`;
+    }
+
+    return await auditRoutes.getAudits({ req, res });
   });
 
   app.get('/audit/user/ofuser/:id', async (req, res) => {
+    const id = req.params.id;
+
+    if (!id) {
+      res.code(400);
+      return {
+        type: 'missing-param-error',
+        msg: 'Please provide a valid id.'
+      };
+    }
+    
     if (!req.query.filter) {
-      req.query.filter = `of_user_id=${req.params.id}`;
+      req.query.filter = `of_user_id=${id}`;
     } else {
-      req.query.filter += `,of_user_id=${req.params.id}`;
+      req.query.filter += `,of_user_id=${id}`;
     }
 
-    return await auditRoutes.getAuditsByUserID({ req, res, ofUser: true });
+    return await auditRoutes.getAudits({ req, res });
   });
 
   app.get('/audit/action/:action', async (req, res) => {
-    return await auditRoutes.getAuditsByType({ action: req.params.action }, res);
+    const action = req.params.action;
+
+    if (!action) {
+      res.code(400);
+      return {
+        type: 'missing-param-error',
+        msg: 'Please provide an action.'
+      };
+    }
+
+    if (!req.query.filter) {
+      req.query.filter = `action=${action}`;
+    } else {
+      req.query.filter += `,action=${action}`;
+    }
+
+    return await auditRoutes.getAudits({ req, res });
   });
 
   app.get('/audit/subaction/:subaction', async (req, res) => {
-    return await auditRoutes.getAuditsByType({ subaction: req.params.subaction }, res);
+    const subaction = req.params.subaction;
+
+    if (!subaction) {
+      res.code(400);
+      return {
+        type: 'missing-param-error',
+        msg: 'Please provide an subaction.'
+      };
+    }
+
+    if (!req.query.filter) {
+      req.query.filter = `subaction=${subaction}`;
+    } else {
+      req.query.filter += `,subaction=${subaction}`;
+    }
+
+    return await auditRoutes.getAudits({ req, res });
   });
 
   app.get('/audit/action/:action/subaction/:subaction', async (req, res) => {
-    return await auditRoutes.getAuditsByType({ action: req.params.action, subaction: req.params.subaction }, res);
+    const action = req.params.action;
+    const subaction = req.params.subaction;
+
+    if (!subaction || !action) {
+      res.code(400);
+      return {
+        type: 'missing-param-error',
+        msg: 'Please provide an action/subaction.'
+      };
+    }
+
+    if (!req.query.filter) {
+      req.query.filter = `action=${action},subaction=${subaction}`;
+    } else {
+      req.query.filter += `,action=${action},subaction=${subaction}`;
+    }
+
+    return await auditRoutes.getAudits({ req, res });
   });
 
   done();
