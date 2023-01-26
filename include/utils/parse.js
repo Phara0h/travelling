f990a5398291adbd5f681d4d95c44daffa379fd3
Phara0h <jtwhissel@gmail.com>
2023-01-26 16:11:20 -0500

    Added x-forward for ip stuff

--- include/utils/parse.js
+++ include/utils/parse.js
@@ -3,7 +3,7 @@ const config = require(__dirname + '/config');
 module.exports = {
   getIp: function getIp(req) {
     if (config.misc.cloudflareIP) {
-      return req.headers['cf-connecting-ip'] || req.headers['CF-Connecting-IP'] || req.headers['CF-CONNECTING-IP'] || req.ip;
+      return req.headers['cf-connecting-ip'] || req.headers['CF-Connecting-IP'] || req.headers['CF-CONNECTING-IP'] || req.headers['x-forwarded-for'] || req.headers['X-Forwarded-For'] || req.ip;
     }
 
     return req.ip;
